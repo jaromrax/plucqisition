93ac87280dd7a529c9a0ba917f8658ae2289ff28
jaromrax <jaromrax@gmail.com>
2015-11-18 17:50:33 +0100

    edie:May2015 commit-autoload

--- online/autoload_libs.C
+++ online/autoload_libs.C
@@ -1,20 +1,13 @@
 {
 #if __x86_64__
   gROOT->ProcessLine("cout<<\" ------------------ 64 BIT SYSTEM \"<<endl");
-  // gSystem->Load("/lib/libdl.so");
-  // gSystem->Load("/lib/x86_64-linux-gnu/libdl.so.2");
-
-  gROOT->ProcessLine("cout<<\" ------------------ 64 BIT SYSTEM LOADED\"<<endl");
+  gROOT->ProcessLine("cout<<\" ------------------ 64 BIT SYSTEM LOADED now\"<<endl");
    
 #else
  gROOT->ProcessLine("cout<<\"-------------------- 32 BIT SYSTEM \"<<endl");
- gSystem->Load("/usr/lib/libdl.so");
+ gROOT->ProcessLine("cout<<\"-------------------- 32 BIT SYSTEM LOADED now\"<<endl");
 #endif
 
-gROOT->ProcessLine(".L ../xml_attr.C+");
-gROOT->ProcessLine(".L ../log_term.C+");
-gROOT->ProcessLine(".L ../mut_queue.C+");
-gROOT->ProcessLine(".L ../acq_core.C+");
 
  gROOT->ProcessLine("cout<<\"-------------------- acq libraries should be loaded now \"<<endl");
 
